8af3fd8440c555e3a5e8a8c7164ef3e765a1e767
ayokunle <ayokunle.fatokimi@decagon.dev>
2021-02-15 18:15:54 +0100

    add OnboardingSlideContent.swift to Model file

---
+++ devAYZCarbonApp/devAYZCarbonApp/Model/OnboardSlideContents.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardSlideContents.swift
+//  devAYZCarbonApp
+//
+//  Created by Ayokunle on 15/02/2021.
+
+import Foundation
+import UIKit
+
+struct SlideContents {
+    let image: UIImage
+    let title: String
+    let body: String
+    
+    static let collection: [SlideContents] = [
+        .init(image: <#T##UIImage#>, title: <#T##String#>, body: <#T##String#>),
+    ]
+}
